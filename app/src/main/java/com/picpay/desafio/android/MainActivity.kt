ce36b26cf3a3699f8958b34e22980c80ff4ecf01
luanfernandes <luanfranciscojr@gmail.com>
2022-05-03 13:37:29 -0400

    Limpando a classed

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.picpay.desafio.android
 
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.picpay.desafio.android.user.UserListAdapter
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
